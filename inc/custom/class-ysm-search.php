447ede15ba2a51e7bf7751bd770838bd26d33e79
Stanislav Samoilenko <samoilenko.stanislav.u@gmail.com>
2022-10-13 20:07:06 +0300

    = 2.4.0 = * Added: hook for altering search results output 'ysm_suggestion_image_output' * Compatibility: WooCommerce 7.0

--- inc/custom/class-ysm-search.php
+++ inc/custom/class-ysm-search.php
@@ -383,12 +383,43 @@ class Ysm_Search {
 
 			/* thumbnail */
 			if ( self::get_var( 'display_icon' ) && has_post_thumbnail( $post ) ) {
+				$the_post = $post;
 
-				$image = get_the_post_thumbnail(
-					$post,
-					apply_filters( 'smart_search_suggestions_image_size', 'post-thumbnail' ),
-					apply_filters( 'smart_search_suggestions_image_attributes', array() )
-				);
+				/**
+				 * Overwrite image html in the suggestion
+				 *
+				 * @param $the_post WP_Post|WC_Product WordPress post object or WooCommerce product object
+				 *
+				 * @since 2.5.0
+				 */
+				$pre_image_output = apply_filters( 'ysm_suggestion_image_output', '', $the_post );
+				if ( ! empty( $pre_image_output ) ) {
+					$image = $pre_image_output;
+				} else {
+					$image = get_the_post_thumbnail(
+						$the_post,
+						/**
+						 * Overwrite image size in the suggestion
+						 * eg. 'post-thumbnail' or 'medium'
+						 *
+						 * @since 2.5.0
+						 */
+						apply_filters( 'ysm_suggestion_image_size',
+							// compatibility with old version
+							apply_filters( 'smart_search_suggestions_image_size', 'post-thumbnail' )
+						),
+						/**
+						 * Overwrite image attributes in the suggestion
+						 * eg. 'post-thumbnail' or 'medium'
+						 *
+						 * @since 2.5.0
+						 */
+						apply_filters( 'ysm_suggestion_image_attributes',
+							// compatibility with old version
+							apply_filters( 'smart_search_suggestions_image_attributes', array() )
+						)
+					);
+				}
 
 				if ( empty( $image ) ) {
 					$post_classes[] = 'smart-search-no-thumbnail';
