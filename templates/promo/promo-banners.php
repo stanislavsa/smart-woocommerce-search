feb26c647c8a19af163f1f375231c6630739fe12
Stanislav Samoilenko <samoilenko.stanislav.u@gmail.com>
2023-08-02 21:02:44 +0300

    = 2.8.1 = * Fixed: issue with creating new widgets

--- templates/promo/promo-banners.php
+++ templates/promo/promo-banners.php
@@ -1,59 +1,32 @@
 <style>
 	.ym-ads-wrap {
 		max-width: 100%;
-		margin: 140px 0 15px 15px;
-		padding: 10px 0;
+		margin: 140px 0 15px 0px;
+		padding: 30px;
 		background: #fff;
 		text-align: center;
 		overflow: hidden;
 	}
 
-	.ym-ads-col {
-		width: 30%;
-		padding: 0 10px;
-		float: left;
-	}
-
-	.ym-ads-wrap img {
-		max-width: 100%;
-	}
-
-	.ym-ads-wrap h3 {
+	.ym-ads-wrap .header {
 		text-align: center;
-		margin: 15px;
+        font-size: 20px;
 	}
 
-	.ym-ads-btn {
-		margin: 15px 0;
-		text-align: center;
-		padding: 15px 25px;
-		background: #1f10d4;
-		display: inline-block;
-		color: #fff;
-		text-decoration: none;
-	}
-
-	.ym-ads-btn:hover {
-		color: #00A0D1;
+	.ym-ads-wrap .ymapp-button {
+		font-size: 14px;
+        margin-left: 0;
+        margin-top: 50px;
 	}
 
 	@media only screen and (max-width: 700px) {
-		.ym-ads-col {
-			width: 100%;
-			padding: 0;
-			float: none;
-		}
+
 	}
 </style>
 <div class="ym-ads-wrap">
-	<div class="ym-ads-col">
-		<h3>Premium Version of Smart Search</h3>
-		<a href="https://yummywp.com/plugins/smart-woocommerce-search/#smart-search-compare" target="_blank">
-			<img src="<?php echo esc_url( plugin_dir_url( __FILE__ ) . '/img/smart-search-ad.png' ); ?>" alt="">
-		</a>
-		<a class="ym-ads-btn" href="https://yummywp.com/plugins/smart-woocommerce-search/#smart-search-compare" target="_blank">
-			GET PRO!
-		</a>
-	</div>
+	<div class="header"><?php esc_html_e( 'Premium version of Smart Search', 'smart-woocommerce-search' ); ?></div>
+	<a href="<?php echo esc_url( 'https://yummywp.com/plugins/smart-woocommerce-search/#smart-search-compare' ); ?>" class="ymapp-button" target="_blank">
+		<?php esc_html_e( 'Upgrade to Pro', 'smart-woocommerce-search' ); ?>
+	</a>
 </div>
 <div class="clear"></div>
